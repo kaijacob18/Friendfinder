2ae5696bb39e382d4588ce8d6ebe3c0584faeaef
kaijacob18 <antionnedavis@gmail.com>
2019-09-10 13:36:41 -0500

    updated

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends = [
+    {
+    friendName: "Rhonda",
+    friendPhoto: "https://amp.businessinsider.com/images/589dbb873149a101788b4c85-960-720.jpg",
+    friendScores:[5,1,4,4,5,1,5,4,1]},
+    {
+    friendName: "Ahmed",
+    friendPhoto: "https://timedotcom.files.wordpress.com/2014/03/happily-surprised.jpg",
+    friendScores:[5,1,4,4,5,1,5,4,1]},
+    {
+    friendName: "Beth",
+    friendPhoto: "https://www.sciencenewsforstudents.org/sites/default/files/scald-image/350_.inline2_beauty_w.png",
+    friendScores:[5,1,4,4,5,1,5,4,1]},
+
+    {
+    friendName: "Jamese",
+    friendPhoto: "https://dwgyu36up6iuz.cloudfront.net/heru80fdn/image/upload/c_fill,d_placeholder_thescene.jpg,fl_progressive,g_face,h_270,q_80,w_270/v1457981865/allure_51f6f2433ccd688fd8df011b.jpg",
+    friendScores:[5,1,4,4,5,1,5,4,1]},
+    {
+    friendName: "Sharon",
+    friendPhoto: "https://dwgyu36up6iuz.cloudfront.net/heru80fdn/image/upload/c_fill,d_placeholder_thescene.jpg,fl_progressive,g_face,h_270,q_80,w_270/v1457982263/allure_51f6f2433ccd688fd8df011f.jpg",
+    friendScores:[5,1,4,4,5,1,5,4,1]},
+    {
+    friendName: "Bill",
+    friendPhoto: "https://mediaslide-europe.storage.googleapis.com/premier/pictures/4498/17325/profile-1531315553-81c526184fa75df1b56adf1751a71d26.jpg",
+    friendScores:[5,1,4,4,5,1,5,4,1]},
+    {
+    friendName: "Roger",
+    friendPhoto: "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1440,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1560810352/190617-sommer-jones-tease_fcfcq2",
+    friendScores:[5,1,4,4,5,1,5,4,1]}
+        
+];
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 //Dependencies
 
 var express = require("express");
-
+var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type:"application/vmd.api+json"}));
+
+// app.use(express.urlencoded({ extended: true}));
+// app.use(express.json());
 
 //Router
+app.use(express.static("app/public"));
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 //Listener
 
